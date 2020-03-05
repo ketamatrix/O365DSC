de351eb9f103e561f64f638a4d4155a2dd81f44f
ketamatrix <61819646+ketamatrix@users.noreply.github.com>
2020-03-05 17:46:08 +1100

    Add files via upload

---
+++ Deploy.ps1
@@ -0,0 +1,3 @@
+﻿winrm quickconfig -force # Configure the Agent to run DSC configurations
+Install-Module Office365DSC -Force -AllowPrerelease # Install the Office365DSC module
+&"D:\a\r1\a\_Showcase-CI\DSC\O365.ps1" # Execute the DSC Configuration script from the built artefacts
---
+++ O365.ps1
@@ -0,0 +1,23 @@
+﻿Configuration O365
+{
+    Import-DscResource -ModuleName Office365DSC
+    $password = ConvertTo-SecureString 'MySecretPassword' -AsPlainText -Force
+    $GlobalAdmin = New-Object System.Management.Automation.PSCredential ('MyGlobalAdmin@office365dsc.onmicrosoft.com', $password)
+    
+    Node localhost
+    {
+        # Add code changes
+    }
+}
+
+$ConfigData = @{
+    AllNodes = @(
+        @{
+            NodeName                    = "localhost"
+            PsDSCAllowPlaintextPassword = $true
+        }
+    )
+}
+
+O365 -ConfigurationData $ConfigData
+Start-DscConfiguration O365 -Wait -Verbose -Force
